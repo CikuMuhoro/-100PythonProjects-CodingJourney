b4d9eaff5024313752bc3244c65248e025c9fb5e
CikuMuhoro <muhorociku@gmail.com>
2025-04-05 15:25:09 +0300

    final part of rpsgame

--- RockPaperScissors/rpsgame.py
+++ RockPaperScissors/rpsgame.py
@@ -6,9 +6,19 @@ class RockPaperScissors:
     def rpsgame():
         allchoices = ["rock", "paper", "scissors"]
 
-        player_pick = input("shoot between rock paper and scissors:")
+        player_pick = input("shoot between rock paper and scissors: ").lower()
         computer_pick = random.choice(allchoices)
-        print(f"computer choise is: {computer_pick}")
+        print(f"computer choice is: {computer_pick}")
+
+        if player_pick == computer_pick:
+            print("its a tie!")
+        elif (computer_pick == "rock" and player_pick == "scissors") or \
+             (computer_pick == "scissors" and player_pick == "paper") or \
+             (computer_pick == "paper" and player_pick == "rock"):
+            print("Computer won!!")
+
+        else:
+            print("You win!!")
 
 
 RockPaperScissors.rpsgame()
