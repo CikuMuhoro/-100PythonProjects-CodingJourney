7257178fdafabbf6324e1f7557a787d15da31e3a
CikuMuhoro <muhorociku@gmail.com>
2025-04-20 23:46:24 +0300

    word sramble

--- WordScramble/word_scrable.py
+++ WordScramble/word_scrable.py
@@ -1,5 +1,27 @@
-class WordScrable:
+import os
+
+
+class WordScramble:
     def __init__(self):
         pass
 
-    def reading_words(self, easy, medium, hard):
+    def reading_words(self):
+
+        with open("easy_words.txt", "r") as file:
+            easy = [word.strip() for word in file]
+            for word in easy:
+                print(word)
+
+        with open("easy_words.txt", "r") as file:
+            medium = [word.strip() for word in file]
+            for word in medium:
+                print(word)
+
+        with open("hard_words.txt", "r") as file:
+            hard = [word.strip() for word in file]
+            for word in easy:
+                print(word)
+
+
+game = WordScramble()
+game.reading_words()
