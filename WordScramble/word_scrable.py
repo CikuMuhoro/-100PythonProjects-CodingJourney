0252b56878ad926d30938e618edf5803d738fe6d
CikuMuhoro <muhorociku@gmail.com>
2025-04-22 01:04:04 +0300

    scrambled words

--- WordScramble/word_scrable.py
+++ WordScramble/word_scrable.py
@@ -5,23 +5,43 @@ class WordScramble:
     def __init__(self):
         pass
 
-    def reading_words(self):
+    def easy_words(self):
 
         with open("easy_words.txt", "r") as file:
             easy = [word.strip() for word in file]
             for word in easy:
-                print(word)
+                return
 
-        with open("easy_words.txt", "r") as file:
+    def medium_words(self):
+        with open("medium_words.txt", "r") as file:
             medium = [word.strip() for word in file]
             for word in medium:
-                print(word)
+                return word
 
+    def hard_words(self):
         with open("hard_words.txt", "r") as file:
             hard = [word.strip() for word in file]
             for word in hard:
-                print(word)
+                return
+
+    def chose_level(self):
+        level = input("What level do you prefer easy,medium or hard?").lower()
+
+        if level == "easy":
+            self.level = "easy_words.txt"
+            print("Easy level. You have up to five attemps for every word.")
+        elif level == "medium":
+            self.level = "medium_words.txt"
+            print("Medium level. You have up to five attemps for every word.")
+        elif level == "hard":
+            self.level = "hard_words.txt"
+            print("Hard level. You have up to five attemps for every word.")
+        else:
+            print("chose again among the three levels")
+
+    def reading_words(self):
 
 
 game = WordScramble()
 game.reading_words()
+game.chose_level()
