ba34e52452991bff7004edf5002e914c3c0f7195
CikuMuhoro <muhorociku@gmail.com>
2025-06-30 02:02:19 +0300

    printing oole searchresults via duckduckgo

--- GoogleSearch/searchot.py
+++ GoogleSearch/searchot.py
@@ -6,9 +6,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://www.google.com/")
+driver.get("https://duckduckgo.com/")
 
 # handle cookies
 try:
@@ -28,3 +29,35 @@ search_box = WebDriverWait(driver, 10).until(
 
 # TYPE IN THE SERCH BAR AND SUBMIT
 search_box.send_keys("learn selenium python" + Keys.ENTER)
+input("Press Enter AFTER solving CAPTCHA (if any) and cookie popup...\n")
+time.sleep(5)
+
+#scroll to load more result
+for i in range (5):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+    
+#find all result container
+results = driver.find_elements(By.CSS_SELECTOR, "[data-testid='result']")   
+
+#Extract title link and snipet from the conainers
+for container in results:
+    try:
+        title=container.find_element(By.CSS_SELECTOR, "[data-testid='result-title-a'] span")
+        link=container.find_element(By.CSS_SELECTOR, "[data-testid='result-title-a']").get_attribute("href")
+        snippet=container.find_element(By.CSS_SELECTOR, "[data-result ='snippet'] span").text
+        
+        print("Title : ", title.text)
+        print("Link : ", link)
+        print("Snippet : ", snippet)
+        print("_"*60)
+        
+    except Exception as e:
+        #skip if some part is mising
+        continue
+    
+#close the driver
+driver.quit(
+        
+    )
+         
